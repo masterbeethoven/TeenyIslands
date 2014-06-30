dac9f84e3d5b4629923298ca0046fa98624514ce
Core <seorisachs@gmail.com>
2014-06-30 17:31:32 -0400

    new background, added different island, played around with bounce script.

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Bounce : MonoBehaviour {
 
-
 	//Vector2 basePosition = Vector2.zero;
 	// Use this for initialization
 	void Start () {
@@ -18,7 +17,11 @@ public class Bounce : MonoBehaviour {
 		
 		//transform.position = basePosition + new Vector2(.25f, Mathf.Cos(Time.time) * 1f);
 		
-		transform.position = new Vector2( 0f,Mathf.Sin(Time.time));
-		//original bounce code
+		//transform.position = new Vector2( 0f,Mathf.Sin(Time.time));
+		//original bounce code, so like use this after playing around with the other.
+
+		transform.position = new Vector2( 50f,0f);
+		//first number is x, second is y
+
 	}
 }
